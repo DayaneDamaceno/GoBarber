3fafa038c0b43708b2b1907d7c65b80d7ccb6b48
Dayane Damaceno <dayane.damaceno@hotmail.com>
2020-07-31 17:48:13 -0300

    criando model e conexão com o banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'BORA CODAR' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Dayane Almeida',
+    email: 'almeida@example.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
