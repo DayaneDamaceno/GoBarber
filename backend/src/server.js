585fed21e4af196227fa1a893b46606c3f6134c6
Dayane Damaceno <dayane.damaceno@hotmail.com>
2020-07-31 14:43:18 -0300

    configurando eslint etc

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-import app from "./app";
+import app from './app';
 
 app.listen(3333, () => {
-  console.log("conexão estabelecida com sucesso!");
+  console.log('conexão estabelecida com sucesso!');
 });
