a32958836c08fc05b36ab6a9a3afec897e2d8638
Dayane Damaceno <dayane.damaceno@hotmail.com>
2020-07-31 19:13:57 -0300

    gerando hash para senha

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -8,13 +8,16 @@ class UserController {
       return res.status(400).json({ error: 'User already exists!' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
+    const { id, name, email, provider, password_hash } = await User.create(
+      req.body
+    );
 
     return res.json({
       id,
       name,
       email,
       provider,
+      password_hash,
     });
   }
 }
