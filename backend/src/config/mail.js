bc66d2d9f084de307699d483af5c98a53c1b430a
Dayane Damaceno <dayane.damaceno@hotmail.com>
2020-08-05 13:53:40 -0300

    enviando email com template

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ae496b44537fff',
+    pass: '9ef003f0d44a96',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
